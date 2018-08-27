4d4fb1b9ec4a3738027d97371062c5b8f6a21d86
Lui Kohl <lui.kohl@gmail.com>
2018-08-28 00:03:10 +0200

    logic is complete, next style

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-class Titles extends React.Component {
-  render () {
-    return (
-      <div>
-        <h1>Weather Checker</h1>
-        <p>Check local temperatures</p>
-      </div>
-    );
-  }
-}
+const Titles = () => (
+  <div>
+    <h1>Weather Checker</h1>
+    <p>Check local temperatures</p>
+  </div>
+);
 
 export default Titles;
